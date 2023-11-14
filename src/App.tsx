cb427d27c4d963395ede26c02fe099e2ee9fd8ad
adamzuch <adamzuchowicz01@gmail.com>
2023-11-14 20:54:16 +1100

    chore: basic token handling and ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
+import { cn } from './utils'
+
+type Token = string
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tokens, setTokens] = useState<Token[]>([])
+
+  console.log(tokens)
+
+  const operators = ['+', '-', '*', '/']
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="p-16">
+      <div className="flex flex-col items-center justify-center gap-6">
+        <input
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          value={tokens.join('')}
+          placeholder="0"
+          onChange={() => {}}
+        />
+        <div className="grid grid-cols-2 grid-rows-1 gap-3">
+          <Button className="bg-red-500 w-fit" onClick={() => setTokens([])}>
+            C
+          </Button>
+
+          <Button
+            className="bg-red-500 w-fit"
+            onClick={() => {
+              const last = tokens[tokens.length - 1]
+              if (last?.length > 1) {
+                setTokens([...tokens.slice(0, -1), last.slice(0, -1)])
+              } else if (last?.length === 1) {
+                setTokens(tokens.slice(0, -1))
+              }
+            }}
+          >
+            CE
+          </Button>
+        </div>
+
+        <div className="grid grid-cols-4 grid-rows-1 gap-3">
+          {operators.map((operator) => (
+            <Button
+              onClick={() => {
+                const last = tokens[tokens.length - 1]
+                if (last !== undefined && !operators.includes(last)) {
+                  setTokens(tokens.concat(operator))
+                }
+              }}
+            >
+              {operator}
+            </Button>
+          ))}
+        </div>
+
+        <div className="grid grid-cols-3 grid-rows-3 gap-3">
+          {numbers.reverse().map((number) => (
+            <Button
+              className=""
+              onClick={() => {
+                const last = tokens[tokens.length - 1]
+                if (last !== undefined && !operators.includes(last)) {
+                  setTokens([...tokens.slice(0, -1), last + String(number)])
+                } else {
+                  setTokens(tokens.concat(String(number)))
+                }
+              }}
+            >
+              {number}
+            </Button>
+          ))}
+        </div>
       </div>
-      <h1 className="text-red-500 text-3xl">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
 export default App
+
+const Button = ({
+  children,
+  className,
+  ...rest
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) => (
+  <button
+    type="button"
+    className={cn(
+      'rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
+      className
+    )}
+    {...rest}
+  >
+    {children}
+  </button>
+)
