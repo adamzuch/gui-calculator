9063f4828f906da3a1ccff6440257274c98a8702
adamzuch <adamzuchowicz01@gmail.com>
2023-11-15 23:32:46 +1100

    test: more basic tests for calculate function

--- src/lib/calculator.spec.ts
+++ src/lib/calculator.spec.ts
@@ -4,9 +4,19 @@ import { calculate } from './calculator'
 
 describe('calculator', () => {
   // TODO: add testing for unhappy paths
-  it('calculates correctly', () => {
-    const expression = ['1', '+', '2', '*', '3', '-', '4']
+  it('calculates correctly with mixed operations', () => {
+    const expressions = [
+      { expression: ['1', '+', '2', '*', '3', '-', '4'], answer: 3 },
+      { expression: ['90', '/', '9', '*', '3', '+', '7'], answer: 37 },
+      { expression: ['126', '-', '50', '*', '3'], answer: -24 },
+      {
+        expression: ['3', '/', '3', '*', '1', '-', '1', '+', '50'],
+        answer: 50,
+      },
+    ]
 
-    expect(calculate(expression)).toBe(3)
+    for (const { expression, answer } of expressions) {
+      expect(calculate(expression)).toBe(answer)
+    }
   })
 })
